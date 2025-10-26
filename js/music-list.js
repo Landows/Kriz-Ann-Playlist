bfff3c190fdc5525bab13c24ea84be6aece2d434
Landows <landocaburnay456@gmail.com>
2025-10-26 21:48:04 +0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,33 @@
+let allMusic = [
+
+    {
+        name: "Paraluman",
+        artist: "Adie",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Superhuman",
+        artist: "Chris Brown",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Dominga",
+        artist: "La Mave Ft. Nateman",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Sure Thing",
+        artist: "Miguel",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Something New",
+        artist: "Zendaya Ft. Chris Brown",
+        img: "music-5",
+        src: "music-5"
+    },
+    ];
